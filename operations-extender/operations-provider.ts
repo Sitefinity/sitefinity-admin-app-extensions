e8317528edde67606c353d67221adeb8de569b6d
Nikolai Novkirishki <novkirishki94@gmail.com>
2018-04-17 15:08:35 +0300

    Using angular Router for navigation

--- operations-extender/operations-provider.ts
+++ operations-extender/operations-provider.ts
@@ -1,27 +1,27 @@
-import { CommandProvider, OperationsData, Command, OPERATIONS_TOKEN, OperationsTarget, UrlService, URL_SERVICE, CommandModel } from "progress-sitefinity-adminapp-sdk/app/api/v1";
+import { CommandProvider, CommandsData, Command, COMMANDS_TOKEN, CommandsTarget, CommandModel, CommandCategory, ExecutionContext } from "progress-sitefinity-adminapp-sdk/app/api/v1";
 import { Observable } from "rxjs";
-import { ClassProvider, Inject } from "@angular/core";
-import { ExecutionContext } from "progress-sitefinity-adminapp-sdk/app/api/v1/operations/execution-context";
+import { ClassProvider, Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 
-/**
- * A custom commad
- */
-const CUSTOM_COMMAND_BASE: any = {
+const CUSTOM_CATEGORY_NAME = "Custom";
+const CUSTOM_COMMAND_BASE: CommandModel = {
     Name: "Custom",
     Title: "Print preview",
     Ordinal: -1,
-    Category: {
-        Name: "Custom",
-        Title: "Custom commands"
-    },
+    Category: CUSTOM_CATEGORY_NAME,
     Description: null,
     Link: null
 };
 
+const CUSTOM_CATEGORY: CommandCategory = {
+    Name: "Custom",
+    Title: "Custom commands"
+};
+
 class DynamicItemIndexCommandProvider implements CommandProvider {
-    getCommands(data: OperationsData): Observable<CommandModel[]> {
+    getCommands(data: CommandsData): Observable<CommandModel[]> {
         const commands = [];
-        if (data.target === OperationsTarget.List && data.dataItem) {
+        if (data.target === CommandsTarget.List && data.dataItem) {
             const previewCommand = Object.assign({}, CUSTOM_COMMAND_BASE);
             previewCommand.token = {
                 type: PrintPreviewCommand,
@@ -35,21 +35,31 @@ class DynamicItemIndexCommandProvider implements CommandProvider {
 
         return Observable.of(commands);
     }
+
+    getCategories(data: CommandsData): Observable<CommandCategory[]> {
+        return Observable.of([CUSTOM_CATEGORY]);
+    }
 }
 
+@Injectable()
 export class PrintPreviewCommand implements Command {   
-    constructor(@Inject(URL_SERVICE) private urlService: UrlService) {}
+    constructor(protected router: Router) {}
 
     execute(context: ExecutionContext): Observable<any> {
         const dataItem = context.data.dataItem;
-        const url = `/print-preview?entitySet=${dataItem.metadata.setName}&id=${dataItem.key}` + (dataItem.provider ? `&provider=${dataItem.provider}` : ``);
-        window.open(this.urlService.getAbsoluteUrl(url), "_blank");
-        return null;
+        const currentQueryParams = {
+            entitySet: dataItem.metadata.setName,
+            id: dataItem.key,
+            provider: dataItem.provider
+        }
+        const url = `/print-preview`;
+        this.router.navigate([url], { queryParams: currentQueryParams });
+        return Observable.of(null);
     }
 }
 
 export const OPERATIONS_PROVIDER: ClassProvider = {
     useClass: DynamicItemIndexCommandProvider,
     multi: true,
-    provide: OPERATIONS_TOKEN
+    provide: COMMANDS_TOKEN
 };

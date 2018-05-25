0b20ab8c9432b1bcd16fc5771ae5e12eea3dff23
gebov <martin.gebov@gmail.com>
2018-05-25 11:14:20 +0300

    add main file documentation

--- __extensions_index.ts
+++ __extensions_index.ts
@@ -5,7 +5,16 @@ import { CustomFieldsModule } from "./custom-fields";
 import { CommandsExtenderModule } from "./commands-extender";
 import { ToolbarExtenderModule } from "./toolbar-extender";
 
+/**
+ * The entry point of the extensions. Each extension needs to have exactly one export of the Extensions interface.
+ * and it needs to be placed in the __extensions_index file.
+ * Here all of the angular modules are returned and are loaded into the main module.
+ */
 export class SamplesExtension implements Extension {
+
+    /**
+     * Gets the NgModules.
+     */
     getNgModules(): Array<any> {
         return [
             GridExtenderModule,

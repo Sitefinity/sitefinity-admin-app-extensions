70b97af1cf16fa0a746a70722d7bbc2c7587b801
Gabriel <belchev.gb@gmail.com>
2018-07-03 15:37:01 +0300

    initial

---
+++ switch-text-direction/switch-text-direction.provider.ts
@@ -0,0 +1,59 @@
+import { Injectable, ClassProvider } from "@angular/core";
+import { ToolBarItemsProvider, ToolBarItem, TOOLBARITEMS_TOKEN } from "progress-sitefinity-adminapp-sdk/app/api/v1";
+
+const RTL_CLASS = "-sf-direction-rtl";
+const LTR_CLASS = "-sf-direction-ltr";
+
+require("!style-loader!css-loader!./switch-text-direction.provider.css");
+
+@Injectable()
+class SwitchTextDirectionProvider implements ToolBarItemsProvider {
+    getToolBarItems(editorHost: any): ToolBarItem[] {
+        const SWITCH_LEFT_TO_RIGHT_TOOL: ToolBarItem = {
+            name: "Left-to-right",
+            tooltip: "Switch text left to right.",
+            ordinal: 7,
+            exec: () => {
+                const elementContainer: HTMLElement = this.findParent(editorHost);
+
+                elementContainer.classList.remove(RTL_CLASS);
+                elementContainer.classList.add(LTR_CLASS);
+            }
+        };
+
+        const SWITCH_RIGHT_TO_LEFT_TOOL: ToolBarItem = {
+            name: "Right-to-left",
+            tooltip: "Switch text right to left.",
+            ordinal: 6,
+            exec: () => {
+                const elementContainer: HTMLElement = this.findParent(editorHost);
+
+                elementContainer.classList.remove(LTR_CLASS);
+                elementContainer.classList.add(RTL_CLASS);
+            }
+        };
+
+        return [SWITCH_LEFT_TO_RIGHT_TOOL, SWITCH_RIGHT_TO_LEFT_TOOL];
+    }
+
+    getToolBarItemsNamesToRemove(): string[] {
+        // If you want to remove some toolbar items return their names as strings in the array. Order is insignificant.
+        // Otherwise return an empty array.
+        // Example: return [ "embed" ];
+        // The above code will remove the embed toolbar item from the editor.
+        return [];
+    }
+
+    private findParent(editorHost): HTMLElement {
+        const editor = editorHost.getKendoEditor();
+        const node = editor.getRange().startContainer.parentElement;
+
+        return node;
+    }
+}
+
+export const SWITCH_TEXT_DIRECTION_PROVIDER: ClassProvider = {
+    multi: true,
+    provide: TOOLBARITEMS_TOKEN,
+    useClass: SwitchTextDirectionProvider
+};
--- toolbar-extender/index.ts
+++ toolbar-extender/index.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { EDITOR_CONFIG_PROVIDER } from "./editor-config-provider";
 import { VIDEO_TOOLBAR_ITEM_PROVIDER } from "./sitefinity-videos-toolbar-item-provider";
+import { SWITCH_TEXT_DIRECTION_PROVIDER } from "../switch-text-direction/switch-text-direction.provider";
 
 /**
  * The toolbar extender module.
@@ -9,7 +10,8 @@ import { VIDEO_TOOLBAR_ITEM_PROVIDER } from "./sitefinity-videos-toolbar-item-pr
 @NgModule({
     providers: [
         EDITOR_CONFIG_PROVIDER,
-        VIDEO_TOOLBAR_ITEM_PROVIDER
+        VIDEO_TOOLBAR_ITEM_PROVIDER,
+        SWITCH_TEXT_DIRECTION_PROVIDER
     ],
     imports: [
         CommonModule

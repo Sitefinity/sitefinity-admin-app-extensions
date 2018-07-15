fc8a4089d61d0eb4d13ed8a6766f8e793b2863cf
Decho Dechev - Progress <ddechev@progress.com>
2018-07-15 17:32:52 +0300

    Added a e2e execution bash script

---
+++ tests/e2e/headless-e2e.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sandboxurl="$1"
+echo "Starting headless E2E test execution targeting $sandboxurl"
+wget -S "${sandboxurl}/08d586c0-8436-4088-96af-006150e38123.aspx?k=C8E7279CD035B23BB9C0F1F954DFF5B3&un=e2euser" -O credentials.txt
+echo 'User created on CI sandbox'
+
+response=`cat credentials.txt`
+regex='username":"(.*.sitefinity.com)'
+[[ $response =~ $regex ]]
+createdUser=${BASH_REMATCH[1]}
+echo "UserName is: $createdUser"
+
+regex='password":"(.*)"'
+[[ $response =~ $regex ]]
+createdPass=${BASH_REMATCH[1]}
+echo "Password is: $createdPass"
+
+npm run e2e -- --params.sfUrl="$sandboxurl" --params.login.username="$createdUser" --params.login.password="$createdPass" --params.browser="chrome" --params.headless
+                    

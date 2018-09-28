41deea650f42177bd48bc69b5f50947a8fca62ca
gebov <martin.gebov@gmail.com>
2018-09-28 09:49:03 +0300

    support for bundle minification & production build

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -4,46 +4,57 @@ const constants = require("./constants");
 
 const extensionsFileName = "./" + constants.indexFileName;
 const extensionsKey = constants.extensionsKey;
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
-module.exports = {
-    cache: true,
-    devtool: "source-map",
-    entry: {
-        [extensionsKey]: extensionsFileName
-    },
+module.exports = function(env, argv) {
+    const config = {
+        cache: true,
+        devtool: "source-map",
+        entry: {
+            [extensionsKey]: extensionsFileName
+        },
 
-    output: {
-        path: path.join(__dirname, "../dist"),
-        filename: "./[name].bundle.js",
-        pathinfo: true
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "../node_modules/progress-sitefinity-adminapp-sdk/wwwroot"),
-        inline: true,
-        port: 3000,
-        historyApiFallback: true,
-        stats: {
-            colors: true, // color is life
-            chunks: false
+        output: {
+            path: path.join(__dirname, "../dist"),
+            filename: "./[name].bundle.js",
+            pathinfo: true
+        },
+        devServer: {
+            contentBase: path.join(__dirname, "../node_modules/progress-sitefinity-adminapp-sdk/wwwroot"),
+            inline: true,
+            port: 3000,
+            historyApiFallback: true,
+            stats: {
+                colors: true, // color is life
+                chunks: false
+            }
+        },
+        plugins: [
+            new ImportPlugin({
+                context: ".",
+                manifest: require("progress-sitefinity-adminapp-sdk/manifest.json")
+            })
+        ],
+
+        resolve: {
+            extensions: [".ts", ".js", ".json", ".html"]
+        },
+
+        module: {
+            rules: [
+                { test: /\.ts$/, use:[ "awesome-typescript-loader", "angular2-template-loader" ] },
+                { test: /\.html$/, use:[ "html-loader" ] },
+                { test: /\.json$/, use:[ "json-loader" ] },
+                { test: /\.css$/, use: [ "css-to-string-loader", "css-loader" ] }
+            ]
         }
-    },
-    plugins: [
-        new ImportPlugin({
-            context: ".",
-            manifest: require("progress-sitefinity-adminapp-sdk/manifest.json")
-        })
-    ],
+    };
 
-    resolve: {
-        extensions: [".ts", ".js", ".json", ".html"]
-    },
-
-    module: {
-        rules: [
-            { test: /\.ts$/, use:[ "awesome-typescript-loader", "angular2-template-loader" ] },
-            { test: /\.html$/, use:[ "html-loader" ] },
-            { test: /\.json$/, use:[ "json-loader" ] },
-            { test: /\.css$/, use: [ "css-to-string-loader", "css-loader" ] }
-        ]
+    if (env.production) {
+        const uglifyConfig = require('./uglify.conf.json');
+        const uglifyPlugin = new UglifyJsPlugin(uglifyConfig);
+        config.plugins.push(uglifyPlugin);
     }
-};
+
+    return config;
+}

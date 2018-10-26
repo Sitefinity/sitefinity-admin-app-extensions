af9f9b7ee2c61d2b77dd2cc8f11b56c4dff4cb9f
PROGRESS\nstefano <Nikolay.Stefanov@progress.com>
2018-10-26 18:02:36 +0300

    remove the main logic to Iris

--- theme/index.ts
+++ theme/index.ts
@@ -1,28 +1,14 @@
 import { NgModule } from "@angular/core";
-import { RouterModule } from "@angular/router";
-import { ReactiveFormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
-import { ThemeComponent } from "./theme.component";
-import { THEME_PROVIDER, ExtThemeProvider } from "./theme-provider";
-
-export const customRoutes = [
-    { path: "theme", component: ThemeComponent }
-];
-
-export const customRouting = RouterModule.forRoot(customRoutes);
+import { THEME_PROVIDER, CustomThemeProvider } from "./theme-provider";
 
 @NgModule({
     providers: [
         THEME_PROVIDER,
-        ExtThemeProvider
-    ],
-    declarations: [
-        ThemeComponent
+        CustomThemeProvider
     ],
     imports: [
-        CommonModule,
-        customRouting,
-        ReactiveFormsModule
+        CommonModule
     ]
 })
 

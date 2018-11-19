f6562665b62b4d056c3eb99339d79d319ed3ea5c
nikistefanov <>
2018-11-19 15:43:14 +0200

    address comments

--- theme/index.ts
+++ theme/index.ts
@@ -1,11 +1,10 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { SAMPLE_THEME_PROVIDER, SampleThemeProvider } from "./theme-provider";
+import { SAMPLE_THEME_PROVIDER } from "./theme-provider";
 
 @NgModule({
     providers: [
-        SAMPLE_THEME_PROVIDER,
-        SampleThemeProvider
+        SAMPLE_THEME_PROVIDER
     ],
     imports: [
         CommonModule

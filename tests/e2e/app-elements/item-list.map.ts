105bbe5863933a35e86c3d1f1c269fbeaad6d989
Decho Dechev <dechoD@users.noreply.github.com>
2018-12-03 14:11:01 +0200

    Reverted changes to css locators. (#70)
    These are not yet checked in to Iris fixes

--- tests/e2e/app-elements/item-list.map.ts
+++ tests/e2e/app-elements/item-list.map.ts
@@ -3,10 +3,10 @@ import { element, by, ElementFinder, ElementArrayFinder } from "protractor";
 export class ItemListMap {
     public static TitleTag: ElementFinder = element.all(by.css(".sf-row h1")).last();
     public static PrintPreviewButton: ElementFinder = element(by.cssContainingText("div[role=option]", "Print preview"));
-    public static CountLabel: ElementFinder = element(by.css("div.sf-row__col.-sf-txt-align-right.-sf-txt-hint"));
+    public static CountLabel: ElementFinder = element(by.css("div.row__col.-txt-align-right.-txt-hint"));
     public static TableHeaders: ElementArrayFinder = element(by.className("sf-tree-list__row -head")).all(by.className("sf-tree-list__cell -head"));
     public static TableElements: ElementArrayFinder = element.all(by.css(".sf-tree-list__row:not(.-head):not(.-loading)"));
-    public static BackButton: ElementFinder = element(by.css(".sf-button.-toggle.-icon[title='All items']"));
+    public static BackButton: ElementFinder = element(by.css(".sf-button.-toggle.-icon[title=Back]"));
 
     public static GetRowTitleCell(rowTitle: string): ElementFinder {
         const itemRow = ItemListMap.GetTableRow(rowTitle);

d6c2c6834ff61833255f1685d7adcb8d61885dff
Gabriel <belchev.gb@gmail.com>
2019-05-20 16:55:41 +0300

    Add vimeo to supported embed types.

---
+++ embed-media/vimeo-media.parser.ts
@@ -0,0 +1,46 @@
+import { EmbedMediaParser, Media, MediaType } from "progress-sitefinity-adminapp-sdk/app/api/v1";
+import { ClassProvider } from "@angular/core";
+
+const VIMEO_URL_REGEX = new RegExp("((?:(?:https?|ftp|file):\/\/|www\.|ftp\.)vimeo.com[/0-9]+)", "i");
+
+/**
+ * Handles pastes and embed media links for Vimeo.
+ */
+export class VimeoMediaParser extends EmbedMediaParser {
+    /**
+     * Determines if the input text contains Vimeo link.
+     * @param inputText
+     */
+    canProcess(inputText: string): boolean {
+        return VIMEO_URL_REGEX.test(inputText);
+    }
+
+    /**
+     * Parses the input text and retrieves HTML to embed Vimeo video.
+     * @param inputText
+     */
+    parse(inputText: string): Promise<Media> {
+        const vimeoUrl = VIMEO_URL_REGEX.exec(inputText)[0];
+        const endpoint = `https://vimeo.com/api/oembed.json?url=${vimeoUrl}`;
+
+        return fetch(endpoint)
+            .then(r => r.json())
+            .then(r => {
+                const media: Media = {
+                    html: r.html,
+                    isValid: true,
+                    height: { value: 300, hasValue: true },
+                    width: { value: 600, hasValue: true },
+                    type: MediaType.IFrame
+                };
+
+                return media;
+            });
+    }
+}
+
+export const VIMEO_EMBED_MEDIA_PROVIDER: ClassProvider = {
+    multi: true,
+    provide: EmbedMediaParser,
+    useClass: VimeoMediaParser
+};

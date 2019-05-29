dec552c8cb543122b27b2dabad584c4736226b3d
Decho Dechev - Progress <ddechev@progress.com>
2019-05-29 17:04:58 +0300

    Fixed an warning

--- tests/e2e/app-elements/editor-popup.map.ts
+++ tests/e2e/app-elements/editor-popup.map.ts
@@ -2,5 +2,5 @@ import { element, by, ElementFinder } from "protractor";
 
 export class EditorPopupMap {
     public static ToolPopup: ElementFinder = element(by.css(".k-ct-popup.symbol-popup"));
-    public static SymbolCell: ElementFinder = element(by.css(".symbol-cell"));
+    public static SymbolCell: ElementFinder = element.all(by.css(".symbol-cell")).first();
 }

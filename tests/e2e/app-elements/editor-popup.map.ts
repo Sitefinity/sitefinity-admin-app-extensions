193fbf2ff6a17a6ba615dc1dda03679ef54be922
Decho Dechev - Progress <ddechev@progress.com>
2019-05-31 09:55:26 +0300

    Added comment on the use of first

--- tests/e2e/app-elements/editor-popup.map.ts
+++ tests/e2e/app-elements/editor-popup.map.ts
@@ -2,5 +2,6 @@ import { element, by, ElementFinder } from "protractor";
 
 export class EditorPopupMap {
     public static ToolPopup: ElementFinder = element(by.css(".k-ct-popup.symbol-popup"));
+    // We use first to avoid logging in console that more than one element was found
     public static SymbolCell: ElementFinder = element.all(by.css(".symbol-cell")).first();
 }

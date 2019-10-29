3f33a9751c2d4f9abeecc71118a8a054a53070ce
Julia Meleshko <julia.meleshko@progress.com>
2019-10-29 16:10:27 +0200

    Array of GUIDs custom field

---
+++ custom-fields/array-of-guids/array-of-guids-write.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { FieldBase } from "progress-sitefinity-adminapp-sdk/app/api/v1";
+
+/**
+ * The component used to display the field in write mode.
+ */
+@Component({
+    templateUrl: "./array-of-guids-write.component.html"
+})
+export class ArrayOfGUIDsWriteComponent extends FieldBase { }

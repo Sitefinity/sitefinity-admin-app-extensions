7621f84af5bd73a1c5168fd0fd037b499a65fc6b
Gabriel <belchev.gb@gmail.com>
2020-06-29 09:54:58 +0300

    Use global __iris_require__

--- build/delegated.module.js
+++ build/delegated.module.js
@@ -25,7 +25,7 @@ class DelegatedModule extends DelegateModuleWebPack {
         if (!sourceModule) {
             str = WebpackMissingModule.moduleCode(this.sourceRequest);
         } else {
-            str = `module.exports = __webpack_require__(0).__iris_require__('${this.userRequest}')`;
+            str = `module.exports = __iris_require__('${this.userRequest}')`;
         }
         return new RawSource(str);
     }

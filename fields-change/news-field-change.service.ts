2c447e0e170e723f9eea35f3fef35ef624fb3547
Gabriel <belchev.gb@gmail.com>
2020-06-30 11:49:41 +0300

    Add comments

--- fields-change/news-field-change.service.ts
+++ fields-change/news-field-change.service.ts
@@ -1,4 +1,4 @@
-import { FieldsChangeService, FieldWrapper, FIELDS_CHANGE_SERVICE_TOKEN } from "progress-sitefinity-adminapp-sdk/app/api/v1";
+import { FieldChangeService, FieldWrapper, FIELDS_CHANGE_SERVICE_TOKEN } from "progress-sitefinity-adminapp-sdk/app/api/v1";
 import { Injectable } from "@angular/core";
 
 const NEWS_TYPE_FULL_NAME = "Telerik.Sitefinity.News.Model.NewsItem";
@@ -6,8 +6,17 @@ const TITLE_FIELD_NAME = "Title";
 const RITCH_TEXT_EDITOR_FIELD_NAME = "Content";
 
 @Injectable()
-export class NewsFieldsChangeService implements FieldsChangeService {
+export class NewsFieldsChangeService implements FieldChangeService {
+    /**
+     * Method called when on of the field changes.
+     *
+     * @param {string} changedFieldName The name of the field that was changed.
+     * @param {*} changedValue The new value of the field.
+     * @param {FieldWrapper[]} fields All fields in the form.
+     * @memberof FieldsChangeService
+     */
     processChange(changedFieldName: string, changedValue: any, fields: FieldWrapper[]): void {
+        // When the title field is changed, we want to update the long text field with the same value(mirroring).
         if (changedFieldName === TITLE_FIELD_NAME) {
             const ritchTextEditorField = fields.find(x => x.fieldModel.key === RITCH_TEXT_EDITOR_FIELD_NAME);
 
@@ -15,6 +24,13 @@ export class NewsFieldsChangeService implements FieldsChangeService {
         }
     }
 
+    /**
+     * Determines if this service can process the changed field.
+     *
+     * @param {string} typeFullName The CLR full name of the item(ex: Telerik.Sitefinity.News.Model.NewsItem)
+     * @returns {boolean} Wheter the field can be processed.
+     * @memberof FieldsChangeService
+     */
     canProcess(typeFullName: string): boolean {
         return typeFullName === NEWS_TYPE_FULL_NAME;
     }

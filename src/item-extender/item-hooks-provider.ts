bb220a2923d1a34fa72a8936d14d028c748683b1
Gabriel <belchev.gb@gmail.com>
2021-02-11 10:18:47 +0200

    sample

--- src/item-extender/item-hooks-provider.ts
+++ src/item-extender/item-hooks-provider.ts
@@ -1,5 +1,6 @@
 import { Injectable, ClassProvider } from "@angular/core";
 import { ItemHooksProvider, ITEM_HOOKS_PROVIDER_TOKEN, DataItem } from "progress-sitefinity-adminapp-sdk/app/api/v1";
+import { Observable, ReplaySubject } from "rxjs";
 
 @Injectable()
 class CustomItemHooksProvider implements ItemHooksProvider {
@@ -11,6 +12,40 @@ class CustomItemHooksProvider implements ItemHooksProvider {
             // tslint:disable-next-line:no-console
             console.log(`A new item is being created`);
         }
+    }
+
+    onEditItemChanged(item: DataItem): Observable<void> {
+        return this.executeAsyncOperation(`Item changed: ${item?.data?.Title || 'No item'}`);
+    }
+
+    onEditItemInitializing(item: DataItem): Observable<void> {
+        return this.executeAsyncOperation(`Item initializing: ${item?.data?.Title || 'No item'}`);
+    }
+
+    onEditItemUnloading(item: DataItem): Observable<void> {
+        return this.executeAsyncOperation(`Item unloading: ${item?.data?.Title || 'No item'}`);
+    }
+
+    onGridItemsChanged(items: DataItem[]): Observable<void> {
+        return this.executeAsyncOperation(`Grid items changing: ${items?.length ? items.map(x => x.data.Title) : 'No items'}`);
+    }
+
+    onGridItemsInitializing(items: DataItem[]): Observable<void> {
+        return this.executeAsyncOperation(`Grid items initializing: ${items?.length ? items.map(x => x.data.Title) : 'No items'}`);
+    }
+
+    onGridItemsUnloading(items: DataItem[]): Observable<void> {
+        return this.executeAsyncOperation(`Grid items unloading: ${items?.length ? items.map(x => x.data.Title) : 'No items'}`);
+    }
+
+    private executeAsyncOperation(message: string): Observable<void> {
+        const result$ = new ReplaySubject<void>();
+
+        setTimeout(() => {
+            console.log(message);
+        }, 1000);
+
+        return result$.asObservable();
     }
 }
 

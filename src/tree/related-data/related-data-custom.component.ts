7455847a3609100ef9f0518daf1e857fa35f17a1
Stefan Koserkov <stefankoserkov@gmail.com>
2021-07-16 15:29:31 +0300

    fix bull point bug and size

--- src/tree/related-data/related-data-custom.component.ts
+++ src/tree/related-data/related-data-custom.component.ts
@@ -3,11 +3,17 @@ import { DataItem } from "@progress/sitefinity-adminapp-sdk/app/api/v1";
 import { CustomTreeNodeComponentBase } from "@progress/sitefinity-adminapp-sdk/app/api/v1/tree";
 
 @Component({
+    styles: [
+        ".bull { font-weight: bolder; }"
+    ],
     template: `
-    <div [textContent]="item.data.Title"><span>&bull;&nbsp;</span> <span data-sftest="custom-created-by" [textContent]="item.data.CreatedBy"></span></div>
+    <div>
+        <span [textContent]="item.data.Title"></span>
+        <span class="bull">&nbsp;&bull;&nbsp;</span>
+        <span data-sftest="custom-created-by" [textContent]="item.data.CreatedBy"></span>
+    </div>
     <div class="sf-tree__description sf-breadcrumb -sf-overflow-ellipsis">
-        <span data-sftest="custom-created-on" [textContent]="item.data.DateCreated | date:'medium'">
-        </span>
+        <span data-sftest="custom-created-on" [textContent]="item.data.DateCreated | date:'medium'"></span>
     </div>
     `
 })

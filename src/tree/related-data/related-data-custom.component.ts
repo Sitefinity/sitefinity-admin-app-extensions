b5230286c9f2a5ab38335c21b125b835853bddff
Nikolai Novkirishki <Novkirishki@users.noreply.github.com>
2021-07-19 12:04:56 +0300

    Revert "fix bull point bug and size (#337)"
    This reverts commit febecd03cc02dbdcfc5a7b73ba6ce1a6cc6c4029.

--- src/tree/related-data/related-data-custom.component.ts
+++ src/tree/related-data/related-data-custom.component.ts
@@ -3,17 +3,11 @@ import { DataItem } from "@progress/sitefinity-adminapp-sdk/app/api/v1";
 import { CustomTreeNodeComponentBase } from "@progress/sitefinity-adminapp-sdk/app/api/v1/tree";
 
 @Component({
-    styles: [
-        ".bull { font-weight: bolder; }"
-    ],
     template: `
-    <div>
-        <span [textContent]="item.data.Title"></span>
-        <span class="bull">&nbsp;&bull;&nbsp;</span>
-        <span data-sftest="custom-created-by" [textContent]="item.data.CreatedBy"></span>
-    </div>
+    <div [textContent]="item.data.Title"><span>&bull;&nbsp;</span> <span data-sftest="custom-created-by" [textContent]="item.data.CreatedBy"></span></div>
     <div class="sf-tree__description sf-breadcrumb -sf-overflow-ellipsis">
-        <span data-sftest="custom-created-on" [textContent]="item.data.DateCreated | date:'medium'"></span>
+        <span data-sftest="custom-created-on" [textContent]="item.data.DateCreated | date:'medium'">
+        </span>
     </div>
     `
 })
